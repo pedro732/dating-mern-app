1359821ba28cfcd8ac1cdae116b2627a95b47908
Pedro Andrade Jiménez <pedroandradejimenez@MacBook-Air-de-Pedro.local>
2025-01-25 21:37:24 -0300

    Modificaciones en el backend , auth.js

--- frontend/src/components/axios.js
+++ frontend/src/components/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}` // Ejemplo de cómo enviar un token de autenticación
+    }
 });
 
 export default instance;
